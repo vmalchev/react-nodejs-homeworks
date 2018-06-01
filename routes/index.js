26ff78587b3a30d5052e7f4b198bf53486464d07
Vasil Malchev <vasil.malchev@picasse.com>
2018-06-01 15:33:55 +0300

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
 const db = require('../db')
@@ -43,12 +45,12 @@ router.get('/posts/:postId', function (req, res) {
 
 router.post('/posts', function (req, res) {
     const post = req.body;
-    db.query('INSERT INTO post(title,text,tags,status,author) VALUES ?"', [[post.title,post.text,post.tags,post.status, post.author]], function (err, result) {
+    db.query('INSERT INTO post SET ?', post, function (err, result) {
         if (err) throw err;
 
-        console.log('Inserting post:', post);
-        if (result.affectedRows === 1) {
-            const uri = req.baseUrl + '/' + post.id;
+        console.log('Inserting post:'+JSON.stringify(post));
+        if (result.affectedRows == 1) {
+            const uri = req.url + '/' + result.insertId;
             console.log('Created Post: ', uri);
             res.location(uri).status(201).json(post);
         } else {
@@ -62,54 +64,83 @@ router.post('/posts', function (req, res) {
             });
         }
     }).catch((err) => {
-        error(req, res, 500, `Server error: ${err}`, err);
+        var message = `Server error: ${err}`;
+        res.status(500);
+        res.json({
+          'error': [{
+            message,
+            error: {}
+          }]
+        });
     })
   });
 
 router.put('/posts/:postId', function (req, res) {
     const post = req.body;
 
-        if (user.id !== req.params.userId) {
-            error(req, res, 400, `Invalid user data - id in url doesn't match: ${user}`);
-            return;
-        }
-
-        console.log('Updating post:', post);
-
-                if (result.modifiedCount === 1) {
-                    res.json(resultUser);
-                } else {
-                    error(req, res, 400, `Data was NOT modified in database: ${JSON.stringify(user)}`);
-                }
-            }).catch((err) => {
-                error(req, res, 500, `Server error: ${err}`, err);
-            })
-    }).catch(errors => {
-        error(req, res, 400, `Invalid user data: ${util.inspect(errors)}`);
-    })
-});
-
-router.delete('/posts/:postId', function (req, res) {
-    const db = req.app.locals.db;
-    const params = req.params;
-    indicative.validate(params, { userId: 'required|regex:^[0-9a-f]{24}$' })
-        .then(() => {
-            db.collection('users', function (err, users_collection) {
-                if (err) throw err;
-                users_collection.findOneAndDelete({ _id: new mongodb.ObjectID(params.userId) },
-                    (err, result) => {
-                        if (err) throw err;
-                        if (result.ok) {
-                            replaceId(result.value);
-                            res.json(result.value);
-                        } else {
-                            error(req, res, 404, `User with Id=${params.userId} not found.`, err);
-                        }
-                    });
-            });
-        }).catch(errors => {
-            error(req, res, 400, 'Invalid user ID: ' + util.inspect(errors))
+    if (post.id !== req.params.postId) {
+        var message = `Invalid user data - id in url doesn't match: ${post}`;
+        res.status(400);
+        res.json({
+          'error': [{
+            message,
+            error: {}
+          }]
         });
-});
+
+        return;
+    }
+
+//     db.query('UPDATE post SET ? WHERE id= :postId', post, function (err, result) {
+//         if (err) throw err;
+//
+//         console.log('Updating post:', post);
+//
+//         if (result.modifiedCount === 1) {
+//             res.json(result);
+//         } else {
+//           var message = `Data was NOT modified in database: ${JSON.stringify(post)}`;
+//             res.status(400);
+//             res.json({
+//               'error': [{
+//                 message,
+//                 error: {}
+//               }]
+//             });
+//         }
+//     }).catch((err) => {
+//         var message = `Server error: ${err}`;
+//         res.status(500);
+//         res.json({
+//           'error': [{
+//             message,
+//             error: {}
+//           }]
+//         });
+//     })
+// });
+//
+// router.delete('/posts/:postId', function (req, res) {
+//     const db = req.app.locals.db;
+//     const params = req.params;
+//     indicative.validate(params, { userId: 'required|regex:^[0-9a-f]{24}$' })
+//         .then(() => {
+//             db.collection('users', function (err, users_collection) {
+//                 if (err) throw err;
+//                 users_collection.findOneAndDelete({ _id: new mongodb.ObjectID(params.userId) },
+//                     (err, result) => {
+//                         if (err) throw err;
+//                         if (result.ok) {
+//                             replaceId(result.value);
+//                             res.json(result.value);
+//                         } else {
+//                             error(req, res, 404, `User with Id=${params.userId} not found.`, err);
+//                         }
+//                     });
+//             });
+//         }).catch(errors => {
+//             error(req, res, 400, 'Invalid user ID: ' + util.inspect(errors))
+//         });
+// });
 
 module.exports = router;
